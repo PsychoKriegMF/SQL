a351a13bcfa867d8966c8f87023636e34bbce0ae
Top <Top@WIN-0VIMVR9QCQR>
2024-09-04 20:41:48 +0500

    Добавьте файлы проекта.

---
+++ SQL/DBWork.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Reflection.Emit;
+
+
+namespace SQL
+{
+    internal class DBWork
+    {
+        static public string MakeDB(string _dbname = "test02")
+        {
+            string result = "Что то пошло не так!";
+            string path = $"Data Source={_dbname};";
+            string init_db = "CREATE TABLE IF NOT EXISTS " +
+                "Category " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "Name VARCHAR);";
+            string init_data = "INSERT INTO " +
+                "Category " +
+                "(Name) " +
+                "VALUES " +
+                "('SportWatch');";
+            string show_all_data = "SELECT * FROM Category;";
+            SQLiteConnection conn = new SQLiteConnection(path);
+            SQLiteCommand cmd01 = conn.CreateCommand();
+            SQLiteCommand cmd02 = conn.CreateCommand();
+            SQLiteCommand cmd03 = conn.CreateCommand();
+            cmd01.CommandText = init_db;
+            cmd02.CommandText = init_data;
+            cmd03.CommandText = show_all_data;
+            conn.Open();
+            cmd01.ExecuteNonQuery();
+            cmd02.ExecuteNonQuery();
+            var reader = cmd03.ExecuteReader();
+            if(reader.HasRows)
+            {
+                result = string.Empty;
+                //reader.FieldCount - колличество полей
+                while(reader.Read())
+                {
+                    result += reader.GetValue(0).ToString();
+                    result += " | ";
+                    result += reader.GetValue(1).ToString();
+                    result += "\n";
+                }
+            }
+            conn.Close();
+            return result;
+        }
+    }
+}
---
+++ SQL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "test.db";
+            Console.WriteLine($"Попытка создать базу {name}");
+            Console.WriteLine(); DBWork.MakeDB(name);
+        }
+    }
+}
